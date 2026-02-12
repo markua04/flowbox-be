03bb383146e2b3b59304820a2f342e0dec1e183b
Aiden Markus <markua04@gmail.com>
2026-02-12 20:43:07 +0100

    Stop tracking dist build output

--- dist/middlewares/asyncHandler.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const asyncHandler = (handler) => {
-    return (req, res, next) => {
-        Promise.resolve(handler(req, res, next)).catch(next);
-    };
-};
-exports.default = asyncHandler;
